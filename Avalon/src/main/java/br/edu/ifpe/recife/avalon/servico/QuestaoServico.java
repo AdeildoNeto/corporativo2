9365f75d55032360b5f8756e4a06093ac3e4fa76
eduardoamaral <eduardoamaral@192.168.25.4>
2018-04-15 00:47:33 -0300

    ID.01 Correção de espações no enunciado da questão

--- Avalon/src/main/java/br/edu/ifpe/recife/avalon/servico/QuestaoServico.java
+++ Avalon/src/main/java/br/edu/ifpe/recife/avalon/servico/QuestaoServico.java
@@ -39,6 +39,7 @@ public class QuestaoServico {
     private EntityManager entityManager;
 
     public void salvar(Questao questao) {
+        questao.setEnunciado(questao.getEnunciado().trim());
         entityManager.persist(questao);
     }
 
@@ -70,7 +71,7 @@ public class QuestaoServico {
     
     public boolean isEnunciadoPorTipoValido(Questao questao){
         TypedQuery<Questao> query = entityManager.createNamedQuery("Questao.PorEnunciadoTipo", Questao.class);
-        query.setParameter("enunciado", questao.getEnunciado());
+        query.setParameter("enunciado", questao.getEnunciado().trim());
         query.setParameter("tipo", questao.getTipo());
         
         if(TipoQuestaoEnum.MULTIPLA_ESCOLHA.equals(questao.getTipo())){
