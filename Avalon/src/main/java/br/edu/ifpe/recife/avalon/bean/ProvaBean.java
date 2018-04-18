8d4fb942e5eecbe1bc3eaf775ac506fe3b597763
eduardoamaral <eduardoamaral@192.168.25.4>
2018-04-17 21:13:09 -0300

    ID.04 Implementado a estrutura do Gerar Prova.

---
+++ Avalon/src/main/java/br/edu/ifpe/recife/avalon/bean/ProvaBean.java
@@ -0,0 +1,141 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.ifpe.recife.avalon.bean;
+
+import br.edu.ifpe.recife.avalon.model.questao.Questao;
+import br.edu.ifpe.recife.avalon.model.questao.TipoQuestaoEnum;
+import br.edu.ifpe.recife.avalon.servico.QuestaoServico;
+import br.edu.ifpe.recife.avalon.servico.UsuarioServico;
+import java.io.Serializable;
+import javax.inject.Named;
+import javax.enterprise.context.SessionScoped;
+import java.util.ArrayList;
+import java.util.List;
+import javax.annotation.PostConstruct;
+import javax.ejb.EJB;
+
+/**
+ *
+ * @author eduardo.f.amaral
+ */
+@Named(value = "provaBean")
+@SessionScoped
+public class ProvaBean implements Serializable {
+
+    @EJB
+    private QuestaoServico questaoServico;
+
+    @EJB
+    private UsuarioServico usuarioServico;
+
+    private List<TipoQuestaoEnum> tipoQuestoes = new ArrayList<>();
+
+    private TipoQuestaoEnum tipoSelecionado = TipoQuestaoEnum.DISCURSIVA;
+
+    private List<Questao> questoes = new ArrayList<Questao>();
+
+    private List<Questao> questoesSelecionadas = new ArrayList<Questao>();
+    
+    private boolean todosSelecionados = false;
+    
+    
+    public ProvaBean() {
+        this.limparTela();
+    }
+
+    /**
+     * Método para inicializar a busca de questões. 
+     */
+    @PostConstruct
+    private void init() {
+        buscarQuestoes();
+    }
+    
+    /**
+     * Método para carregar as questões do usuário.
+     */
+    private void buscarQuestoes() {
+        this.questoes = questaoServico.buscarQuestoesPorCriadorTipo(1l, tipoSelecionado);
+    }
+
+    /**
+     * Método responsável por carregar os tipos de questão disponíveis.
+     */
+    private void carregarTiposQuestao() {
+        this.tipoQuestoes.add(TipoQuestaoEnum.DISCURSIVA);
+        this.tipoQuestoes.add(TipoQuestaoEnum.MULTIPLA_ESCOLHA);
+        this.tipoQuestoes.add(TipoQuestaoEnum.VERDADEIRO_FALSO);
+    }
+
+    /**
+     * Método para limpar os campos da tela.
+     */
+    private void limparTela() {
+        todosSelecionados = false;
+    }
+    
+    /**
+     * Método para selecionar uma questão da lista de questões.
+     * @param questao 
+     */
+    public void selecionarQuestao(Questao questao){
+        if(questao.isSelecionada()){
+            questoesSelecionadas.add(questao);
+        }else{
+            questoesSelecionadas.remove(questao);
+        }
+    }
+    
+    public void selecionarTipoQuestao(){
+        buscarQuestoes();
+    }
+    
+    public void selecionarTodos(){
+        
+        for(Questao questao : questoes){
+            questao.setSelecionada(todosSelecionados);
+            selecionarQuestao(questao);
+        }
+        
+    }
+    
+    /*
+        GETTERS AND SETTERS
+    */
+    
+    public List<TipoQuestaoEnum> getTipoQuestoes() {
+        return tipoQuestoes;
+    }
+
+    public void setTipoQuestoes(List<TipoQuestaoEnum> tipoQuestoes) {
+        this.tipoQuestoes = tipoQuestoes;
+    }
+
+    public TipoQuestaoEnum getTipoSelecionado() {
+        return tipoSelecionado;
+    }
+
+    public void setTipoSelecionado(TipoQuestaoEnum tipoSelecionado) {
+        this.tipoSelecionado = tipoSelecionado;
+    }
+
+    public List<Questao> getQuestoes() {
+        return questoes;
+    }
+
+    public void setQuestoes(List<Questao> questoes) {
+        this.questoes = questoes;
+    }
+
+    public boolean isTodosSelecionados() {
+        return todosSelecionados;
+    }
+
+    public void setTodosSelecionados(boolean todosSelecionados) {
+        this.todosSelecionados = todosSelecionados;
+    }
+    
+}
