7a985ff94f3c23dc1d749abbcd1ea13e79fa03dc
EduardoAmaral <eduardofsamaral@gmail.com>
2018-10-12 17:29:10 -0300

    ID.105 Ajuste de layout da prova.

--- Avalon/src/main/java/br/edu/ifpe/recife/avalon/viewhelper/PdfGeneratorViewHelper.java
+++ Avalon/src/main/java/br/edu/ifpe/recife/avalon/viewhelper/PdfGeneratorViewHelper.java
@@ -12,9 +12,10 @@ import com.itextpdf.text.BadElementException;
 import com.itextpdf.text.Chunk;
 import com.itextpdf.text.Document;
 import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.Font;
 import com.itextpdf.text.Image;
+import com.itextpdf.text.PageSize;
 import com.itextpdf.text.Paragraph;
-import com.itextpdf.text.Rectangle;
 import com.itextpdf.text.pdf.BaseFont;
 import com.itextpdf.text.pdf.PdfWriter;
 import java.io.IOException;
@@ -31,53 +32,79 @@ import java.util.logging.Logger;
 public class PdfGeneratorViewHelper implements Serializable {
     
     Document document;
+    Font font;
     
     public void gerarArquivo(OutputStream path, List<Questao> questoes){
         try {
-            Rectangle pagesize = new Rectangle(794f, 1123f);
-            document = new Document(pagesize, 72f, 48f, 72f, 36f);
+            document = new Document(PageSize.A4);
             PdfWriter.getInstance(document, path);
             
             document.open();
             
+            definirFonte();
             imprimirCabecalho();
+            imprimirTitulo();
             imprimirQuestoes(questoes);
             
             document.close();
             
-        } catch (DocumentException ex) {
-            Logger.getLogger(PdfGeneratorViewHelper.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (IOException ex) {
+        } catch (DocumentException | IOException ex) {
             Logger.getLogger(PdfGeneratorViewHelper.class.getName()).log(Level.SEVERE, null, ex);
         }
         
     }
     
+    private void definirFonte(){
+        font = new Font(Font.FontFamily.TIMES_ROMAN);
+    }
+    
     private void imprimirCabecalho() throws DocumentException, IOException{
         Image cabecalho = Image.getInstance("https://localhost:8181/Avalon/resources/img/cabecalho.png");
             
-        cabecalho.scalePercent(50.0f, 50.0f);
+        cabecalho.scalePercent(40.5f, 40.5f);
         
         document.add(cabecalho);
+    }
+    
+    private void imprimirTitulo() throws DocumentException{
+        Font fontTitulo = new Font(Font.FontFamily.TIMES_ROMAN, 12.0f, Font.BOLD);
+        Paragraph titulo = new Paragraph("Avaliação", fontTitulo);
+        titulo.setAlignment(Paragraph.ALIGN_CENTER);
+        document.add(titulo);
+        document.add(Chunk.NEWLINE);
     }
     
     private void imprimirQuestoes(List<Questao> questoes) throws DocumentException, IOException{
         Questao questao;
         for (int i = 0; i < questoes.size(); i++) {
             questao = questoes.get(i);
-            document.add(new Paragraph(questao.formatarEnunciado(i + 1)));
+            
+            imprimirEnunciado(questao, i + 1);
+            
+            if(questao.getImagem() != null){
+                imprimirImagem(questao);
+            }
+            
             imprimirAlternativas(questao);
             document.add(Chunk.NEWLINE);
         }
         
     }
     
+    private void imprimirEnunciado(Questao questao, int numero) throws DocumentException{
+        Paragraph enunciado = new Paragraph(questao.formatarEnunciado(numero), font);
+        document.add(enunciado);
+    }
     
+    private void imprimirImagem(Questao questao) throws IOException, BadElementException, DocumentException{
+        Image imagem = Image.getInstance(questao.getImagem().getArquivo());
+        document.add(imagem);
+    }
 
     private void imprimirAlternativas(Questao questao) throws DocumentException, IOException {
         if(questao instanceof VerdadeiroFalso || questao instanceof MultiplaEscolha){
-            float indentation = BaseFont.createFont().getWidthPoint(questao.formatarQuestao(), 1);
-            Paragraph alternativas = new Paragraph(questao.formatarQuestao());
+            float indentation = BaseFont.createFont().getWidthPoint(questao.formatarQuestao(), 1.5f);
+            Paragraph alternativas = new Paragraph(questao.formatarQuestao(), font);
             alternativas.setIndentationLeft(indentation);
             
             document.add(alternativas);
